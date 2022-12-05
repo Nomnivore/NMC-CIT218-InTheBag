595b6df8f52d788a66569d690845d3feaa6f3b68
nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-12-05 01:23:09 -0500

    hands on 1 and 2

---
+++ Controllers/GenieController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace InTheBag.Controllers
+{
+    public class GenieController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Create(string GenieName, int Age, int WishesGranted)
+        //{
+        //    if (WishesGranted > 5000 || Age > 1000)
+        //        return View("ExperiencedGenie");
+        //    else
+        //        return View("Novice");
+        //}
+
+        [HttpPost]
+        public IActionResult Create(string GenieName)
+        {
+            int numGranted = Int32.Parse(Request.Form["WishesGranted"]);
+            int Years = Int32.Parse(Request.Form["Age"]);
+
+            if (numGranted > 5000 || Years > 1000)
+                return View("ExperiencedGenie");
+            else
+                return View("Novice");
+        }
+    }
+}
